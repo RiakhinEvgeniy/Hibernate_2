0269075219fe41eb64e242e8e19f3b6d72b12313
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-06-20 23:21:25 +0200

    Added class SessionCreator

---
+++ src/main/java/com/riakhin/MyRunner.java
@@ -0,0 +1,9 @@
+package com.riakhin;
+
+import com.riakhin.config.SessionCreator;
+
+public class MyRunner {
+    public static void main(String[] args) {
+        SessionCreator sessionCreator = new SessionCreator();
+    }
+}
---
+++ src/main/java/com/riakhin/config/SessionCreator.java
@@ -0,0 +1,15 @@
+package com.riakhin.config;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionCreator {
+
+    public void getSession() {
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory()) {
+        }
+
+        //return sessionFactory.openSession();
+    }
+}
